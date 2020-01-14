b29215afafb3add9dfa1d6fa2421c8ef40ca4f2d
OMGparticles <BlacRyu@users.noreply.github.com>
2020-01-13 20:29:26 -0800

    Refactoring + Cleanup
    - Separated prism and orb logic into two dummy parent layers.

--- scripts/jsmodules/OMGMatrix.js
+++ scripts/jsmodules/OMGMatrix.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// matrix function reference: https://github.com/mrdoob/three.js/blob/dev/src/math/Matrix4.js
+// matrix functions referenced from https://github.com/mrdoob/three.js/blob/dev/src/math/Matrix4.js
 
 const deg2rad = Math.PI / 180;
 const rad2deg = 180 / Math.PI;
@@ -8,26 +8,21 @@ const rad2deg = 180 / Math.PI;
 function clamp(x, min, max){ return Math.max(Math.min(x, max), min) }
 
 export default class Mat4 {
-	constructor(b, type, order){
+	constructor(b){
 		if (b instanceof Mat4){
 			this.elements = new Array(4);
 			for(let i = 0; i < 4; i++){
 				this.elements[i] = b.elements[i].slice();
 			}
 		}
-		else if (b instanceof Vec3 && type && type.toLowerCase() === "euler"){
+		else if (arguments.length === 16){
 			this.elements = new Array(4);
 			for(let i = 0; i < 4; i++){
-				this.elements[i] = new Array(4); 
+				this.elements[i] = new Array(4);
+				for(let j = 0; j < 4; j++){
+					this.elements[i][j] = arguments[i*4 + j];
+				}
 			}
-			this.fromEuler(b, order);
-		}
-		else if (b instanceof Vec3 && type && type.toLowerCase() === "position"){
-			this.elements = new Array(4);
-			for(let i = 0; i < 4; i++){
-				this.elements[i] = new Array(4); 
-			}
-			this.fromPosition(b, order);
 		}
 		else{
 			this.elements = [
@@ -52,45 +47,42 @@ export default class Mat4 {
 
 
 	multiply(right){
-		let m = this.elements;
-		let copy = new Mat4(this);
-		let a = copy.elements;
+		let a = this.elements;
 		let b = right.elements;
+		let r = new Mat4();
 
 		for (let x = 0; x < 4; x++){
 			for(let y = 0; y < 4; y++){
-				m[x][y] = a[x][0] * b[0][y] + a[x][1] * b[1][y] + a[x][2] * b[2][y] + a[x][3] * b[3][y];
+				r.elements[x][y] = a[x][0] * b[0][y] + a[x][1] * b[1][y] + a[x][2] * b[2][y] + a[x][3] * b[3][y];
 			}
 		}
-		return this;
+		return r;
 	}
 
 
-	fromAxisAngle(axis, angle){
+	static fromAxisAngle(axis, angle){
 		let cos = Math.cos(angle);
 		let sin = Math.sin(angle);
 		let t = 1 - cos;
 		let x = axis.x, y = axis.y, z = axis.z;
 		let tx = t*x, ty = t*y;
 
-		this.elements = [
-		[tx * x + cos, tx * y - sin * z, tx * z + sin * y, 0],
-		[tx * y + sin * z, ty * y + cos, ty * z - sin * x, 0],
-		[tx * z - sin * y, ty * z + sin * x, t * z * z + cos, 0],
-		[0, 0, 0, 1]
-		]
+		return new Mat4(
+			tx * x + cos,      tx * y - sin * z,  tx * z + sin * y,  0,
+			tx * y + sin * z,  ty * y + cos,      ty * z - sin * x,  0,
+			tx * z - sin * y,  ty * z + sin * x,  t * z * z + cos,   0,
+			0,                 0,                 0,                 1
+		);
 	}
 
 
-	fromPosition(pos){
-		this.elements = [
-			[1, 0, 0, pos.x],
-			[0, 1, 0, pos.y],
-			[0, 0, 1, pos.z],
-			[0, 0, 0, 1]
-		];
-
-		return this;
+	static fromPosition(pos){
+		return new Mat4(
+			1, 0, 0, pos.x,
+			0, 1, 0, pos.y,
+			0, 0, 1, pos.z,
+			0, 0, 0, 1
+		);
 	}
 
 
@@ -100,9 +92,9 @@ export default class Mat4 {
 	}
 
 
-	fromEuler(angles, order){
+	static fromEuler(angles, order){
 		// XYZ rotation order
-		let m = this.elements;
+		let r = new Mat4();
 		let e = angles.multiply(deg2rad);
 		let sx = Math.sin(e.x), sy = Math.sin(e.y), sz = Math.sin(e.z);
 		let cx = Math.cos(e.x), cy = Math.cos(e.y), cz = Math.cos(e.z);
@@ -115,46 +107,46 @@ export default class Mat4 {
 
 			case "XYZ":
 			default:
-				m[0][0] = cy * cz;
-				m[0][1] = -cy * sz;
-				m[0][2] = sy;
+				r.elements[0][0] = cy * cz;
+				r.elements[0][1] = -cy * sz;
+				r.elements[0][2] = sy;
 
-				m[1][0] = cx * sz + sx * cz * sy;
-				m[1][1] = cx * cz - sx * sz * sy;
-				m[1][2] = -sx * cy;
+				r.elements[1][0] = cx * sz + sx * cz * sy;
+				r.elements[1][1] = cx * cz - sx * sz * sy;
+				r.elements[1][2] = -sx * cy;
 
-				m[2][0] = sx * sz - cx * cz * sy;
-				m[2][1] = sx * cz + cx * sz * sy;
-				m[2][2] = cx * cy;
+				r.elements[2][0] = sx * sz - cx * cz * sy;
+				r.elements[2][1] = sx * cz + cx * sz * sy;
+				r.elements[2][2] = cx * cy;
 				break;
 
 			case "ZYX":
-				m[0][0] = cy * cz;
-				m[0][1] = sx * cz * sy - cx * sz;
-				m[0][2] = cx * cz * sy + sx * sz;
+				r.elements[0][0] = cy * cz;
+				r.elements[0][1] = sx * cz * sy - cx * sz;
+				r.elements[0][2] = cx * cz * sy + sx * sz;
 
-				m[1][0] = cy * sz;
-				m[1][1] = sx * sz * sy + cx * cz;
-				m[1][2] = cx * sz * sy - sx * cz;
+				r.elements[1][0] = cy * sz;
+				r.elements[1][1] = sx * sz * sy + cx * cz;
+				r.elements[1][2] = cx * sz * sy - sx * cz;
 
-				m[2][0] = -sy;
-				m[2][1] = sx * cy;
-				m[2][2] = cx * cy;
+				r.elements[2][0] = -sy;
+				r.elements[2][1] = sx * cy;
+				r.elements[2][2] = cx * cy;
 				break;
 		}
 
-		// right column
-		m[0][3] = 0;
-		m[1][3] = 0;
-		m[2][3] = 0;
+		// right colur.elementsn
+		r.elements[0][3] = 0;
+		r.elements[1][3] = 0;
+		r.elements[2][3] = 0;
 		
-		// bottom row
-		m[3][0] = 0;
-		m[3][1] = 0;
-		m[3][2] = 0;
-		m[3][3] = 1;
+		// bottor.elements row
+		r.elements[3][0] = 0;
+		r.elements[3][1] = 0;
+		r.elements[3][2] = 0;
+		r.elements[3][3] = 1;
 
-		return this;
+		return r;
 	}
 
 
